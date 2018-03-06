6290c0da9e58067277eec041d3b48a3805b76b93
oldironhorse <s1m0n.r3dd1ng@gmail.com>
2018-03-06 09:59:18 +0000

    checker.get_words complete.

--- scrabble/checker/checker.py
+++ scrabble/checker/checker.py
@@ -1,3 +1,16 @@
+from functools import reduce
+
+def words(lines):
+  return {w for w 
+            in reduce(lambda r1, r2: r1 + r2, 
+                      [r.split(' ') for r in lines]) 
+            if len(w) > 1}
+
 def get_words(board):
-  words = {w for w in {r.strip() for r in board} if len(w) > 1}
-  return words
+  cols = []
+  for col in range(0, 15):
+    column = []
+    for row in range(0, 15):
+      column.append(board[row][col])
+    cols.append(''.join(column))
+  return words(board) | words(cols)
--- scrabble/checker/test/test_checker.py
+++ scrabble/checker/test/test_checker.py
@@ -12,7 +12,80 @@ class TestGetWords(TestCase):
       + new_board()[7:]
     self.assertEqual({'remove'}, get_words(b))
 
+  def test_multiple_horizontal_words(self):
+    b = new_board()[:6] \
+      + ('    remove     ',) \
+      + new_board()[7:9] \
+      + ('  dog          ',) \
+      + new_board()[10:]
+    self.assertEqual({'remove', 'dog'}, get_words(b))
 
-  #TODO: multiple words on multiple rows
-  #TODO: multiple words on a single row
-  #TODO: wrods in columns
+  def test_multiple_horizontal_words_single_row(self):
+    b = new_board()[:6] \
+      + ('    remove dog ',) \
+      + new_board()[7:]
+    self.assertEqual({'remove','dog'}, get_words(b))
+
+  def test_single_vertical_word(self):
+    b = new_board()[:6] \
+      + ('    r          ',
+         '    e          ',
+         '    m          ',
+         '    o          ',
+         '    v          ',
+         '    e          ') \
+      + new_board()[12:]
+    self.assertEqual({'remove'}, get_words(b))
+
+  def test_multiple_vertical_words(self):
+    b = new_board()[:5] \
+      + ('         d     ',
+         '    r    o     ',
+         '    e    g     ',
+         '    m          ',
+         '    o          ',
+         '    v          ',
+         '    e          ') \
+      + new_board()[12:]
+    self.assertEqual({'remove', 'dog'}, get_words(b))
+
+  def test_multiple_vertical_words_single_column(self):
+    b = new_board()[:2] \
+      + ('    d          ',
+         '    o          ',
+         '    g          ',
+         '               ',
+         '    r          ',
+         '    e          ',
+         '    m          ',
+         '    o          ',
+         '    v          ',
+         '    e          ') \
+      + new_board()[12:]
+    self.assertEqual({'remove', 'dog'}, get_words(b))
+
+  def test_mixed_words(self):
+    b = new_board()[:5] \
+      + ('               ',
+         '    r          ',
+         '    e          ',
+         '    m          ',
+         '   dog         ',
+         '    v          ',
+         '    e          ') \
+      + new_board()[12:]
+    self.assertEqual({'remove', 'dog'}, get_words(b))
+
+  def test_mixed_words_implied(self):
+    b = new_board()[:5] \
+      + ('               ',
+         '    r          ',
+         '    e          ',
+         '    m          ',
+         '   dog         ',
+         '   ovoid       ',
+         '    e          ') \
+      + new_board()[12:]
+    self.assertEqual({'remove', 'dog', 'do', 'go', 'ovoid'}, get_words(b))
+
+  #TODO: mixed words, incl. implied 2-letter words
